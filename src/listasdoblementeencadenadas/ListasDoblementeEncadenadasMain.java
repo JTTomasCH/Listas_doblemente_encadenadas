0d67f20b20961e8a8bc5e7bca1f7ee9c642e6f27
JTTomasCH <141368999+JTTomasCH@users.noreply.github.com>
2023-09-04 15:08:00 -0500

    Add files via upload

---
+++ src/listasdoblementeencadenadas/ListasDoblementeEncadenadasMain.java
@@ -0,0 +1,66 @@
+
+package listasdoblementeencadenadas;
+
+import listasdoblementeencadenadas.Persona;
+
+
+/**
+ *
+ * @author Tomás Chiriví
+ */
+public class ListasDoblementeEncadenadasMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ListasDoblementeEncadenadas<Persona> personas = new ListasDoblementeEncadenadas<Persona>();
+        
+        System.out.println("La lista esta vacia? "+personas.isEmpty()+"\n");
+        Persona per1 = new Persona("Jorge", 15);
+        Persona per2 = new Persona("Tomas", 32);
+        personas.addLast(per1);
+        personas.addLast(per2);
+        System.out.println("Lista 1: ");
+        for (Persona elem : personas) {
+            System.out.println(elem);
+        }
+        
+        
+        System.out.println("");
+        
+
+        ListasDoblementeEncadenadas<Persona> persona = new ListasDoblementeEncadenadas<Persona>();
+        Persona per3 = new Persona("Jorge", 15);
+        Persona per4 = new Persona("Tomas", 32);
+        persona.addLast(per3);
+        persona.addLast(per4);
+        
+        System.out.println("Lista 2: ");
+        
+        for (Persona elem : persona) {
+            System.out.println(elem);
+        }
+        System.out.println("");
+
+        System.out.println("La lista 1 y 2 son iguales? "+persona.SonIguales(personas)+"\n");
+        
+        
+        Persona per5 = new Persona("Carlos", 15);
+        Persona per6 = new Persona("Luisa", 32);
+        Persona per7 = new Persona("Santiago", 20);
+        persona.addLast(per5);
+        persona.addLast(per6);
+        persona.addFirst(per7);
+        
+        System.out.println("Lista 3: ");
+        for (Persona elem : persona) {
+            System.out.println(elem);
+        }
+        System.out.println("");
+        
+        System.out.println("Tamanio de la lista #3: "+persona.size());
+        System.out.println("Buscar Tomas,32: "+persona.search(per4));
+    }
+    
+}
---
+++ src/listasdoblementeencadenadas/Persona.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listasdoblementeencadenadas;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author SG702-21
+ */
+public class Persona implements Comparable<Persona> {
+    
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + '}';
+    }
+
+    
+    @Override
+    public int compareTo(Persona p) {
+        int resultado=0;
+        if (this.edad<p.getEdad())
+            return -1;
+        else
+            if (this.edad>p.getEdad())
+                return 1;
+        return 0;
+    }
+    
+    public boolean equals(Object contenido) {
+        if (this == contenido) {
+            return true;
+        }
+        if (contenido == null || getClass() != contenido.getClass()) {
+            return false;
+        }
+        Persona persona = (Persona) contenido;
+        return edad == persona.edad && Objects.equals(nombre, persona.nombre);
+    }
+        
+}
